cec4433f4f677bc227fba1c73df7c0689c8ffd8a
akshaymore7610 <107389010+akshaymore7610@users.noreply.github.com>
2022-06-21 12:21:34 +0530

    Add files via upload

---
+++ Practical/Nastedswitch.java
@@ -0,0 +1,39 @@
+class Nastedswitch{
+
+public static void main(String[] args){
+
+char branch= 'e';
+int year = 3;
+
+switch(year)
+{
+case 1:
+System.out.println("english,maths,science");
+break;
+case 2:
+       switch(branch){
+       case 'c':
+       System.out.println("c++");
+       break;
+       case 'e':
+       System.out.println("digital electronics");
+       break;
+       case 'm':
+       System.out.println("mechanics");
+       break;
+       }
+case 3:
+       switch(branch){
+       case 'c':
+       System.out.println("java");
+       break;
+       case 'e':
+       System.out.println("electronics device");
+       break;
+       case 'm':
+       System.out.println("fundamentle of engineering");
+       break;
+       }
+}
+}
+}
