39313f7b0a19e3065003a7b388614fa17ba467a1
akshaymore7610 <107389010+akshaymore7610@users.noreply.github.com>
2022-06-24 11:33:57 +0530

    Add files via upload

---
+++ Lab Activity/LargestNumber.java
@@ -0,0 +1,27 @@
+
+
+public class LargestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 2;
+int b = 4;
+int c = 6;
+int d = 8;
+if (a>b && a>c && a>d)
+{
+	System.out.println("A is largest number");
+}
+else if (b>c && b>d)
+{
+	System.out.println("B is largest number");
+}
+else if (c>d)
+{
+	System.out.println("C is largest number");
+}
+else {
+	System.out.println("D is largest number");
+}
+	}
+	}
