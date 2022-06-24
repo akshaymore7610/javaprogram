ea39618af32186755581366832d9b547c475372c
akshaymore7610 <107389010+akshaymore7610@users.noreply.github.com>
2022-06-24 12:42:20 +0530

    Add files via upload

---
+++ Lab Activity/StarPattern.java
@@ -0,0 +1,31 @@
+ class StarPattern {
+
+	public static void main(String[] args) {
+		pyramid();   
+		
+	}
+
+	private static void pyramid() {
+		    
+		
+        int i, j, row = 5;       
+		 
+	for (i=0; i<row; i++)   
+	{  
+		    
+	for (j=2*(row-i); j>=0; j--)         
+	{  
+		   
+	System.out.print(" ");   
+	}   
+		  
+	for (j=0; j<=i; j++ )   
+	{   
+		  
+	System.out.print("* ");   
+	}   
+		 
+	System.out.println();   
+	}
+	}
+        }
