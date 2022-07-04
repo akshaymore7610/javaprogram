52fdafe12539cfbab806fb4f6671c38923e0f49f
akshaymore7610 <107389010+akshaymore7610@users.noreply.github.com>
2022-07-04 10:03:58 +0530

    Add files via upload

---
+++ Lab Activity/OddEven.java
@@ -0,0 +1,24 @@
+/*
+ *@ Author Akshay More
+ */
+
+class OddEven {
+
+	public static void main(String[] args) {
+		
+ int a[] = {1,2,3,4,5};
+     System.out.println("Even number");
+     for(int i = 0;i<5;i++)
+     {
+    	 if(a[i]%2==0)
+    		 System.out.println(a[i]);
+     }
+     System.out.println("Odd number");
+     for(int i =0;i<5;i++)
+     {
+    	 if(a[i]%2!=0)
+    		 System.out.println(a[i]);
+     }
+	}
+
+}
