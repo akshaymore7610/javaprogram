a3571f3440e95077f33be4d01aa26202a1409c00
akshaymore7610 <107389010+akshaymore7610@users.noreply.github.com>
2022-07-28 18:37:25 +0530

    Add files via upload

---
+++ Lab Activity/StringBufferProgram.java
@@ -0,0 +1,20 @@
+/*
+ * @Author Akshay More
+ */
+
+package weekeight;
+
+	public class StringBufferProgram {
+
+		public static void main(String[] args) {
+			StringBuffer sb = new StringBuffer("This is StringBuffer");
+			System.out.println("New StringBuffer:\n"+sb.append(" This is a sample program"));
+			System.out.println("\n"+sb.insert(21,"Object "));
+			System.out.println("\nReverses String is: "+sb.reverse());
+			
+			System.out.println("sb.");
+			
+
+		}
+
+	}
